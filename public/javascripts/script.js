4ef1ac402d74e4d6f8889f0bd4fec899252c55fe
zmct <zmct99@gmail.com>
2020-08-02 00:32:07 +0000

    Improved layout on mobile

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,32 +1,37 @@
 window.onload = function() {
-    const canvas = document.getElementById('matrix');
+    var canvas = document.getElementById('matrix');
     const ctx = canvas.getContext('2d');
-    const w = canvas.width;
-    const h = canvas.height;
+    const w = screen.width;
+    const h = screen.height;
+    canvas.width = w;
+    canvas.height = h;
     const charWidth = 20;
     const charHeight = 16;
     const cols = Math.floor(w / charWidth) + 1;
     const rows = Math.floor(h / charHeight) + 1;
 
+    // Initialise arrays for code rain
     var chars = Array(cols);
     for (var i = 0; i < cols; i++) {
         chars[i] = new Array(rows);
         for (var j = 0; j < rows; j++) {
-            chars[i][j] = [127, 255];
+            chars[i][j] = [32, 255];
         }
     }
-    var rain = Array(Math.floor(cols / 10));
+    var rain = Array(10); // Array(Math.floor(cols / 10));
     for (var i = 0; i < rain.length; i++) {
         rain[i] = [Math.round(Math.random() * cols), Math.round(-1 * Math.random() * rows)];
     }
 
     ctx.font = '16px monospace';
 
+    // Draw the grid to form the code rain
     function drawGrid() {
         ctx.clearRect(0, 0, w, h);
 
         for (var i = 0; i < cols; i++) {
             for (var j = 0; j < rows; j++) {
+		// Find rain droplets
                 var rainy = false;
                 for (var k = 0; k < rain.length; k++) {
                     if (rain[k][0] == i && rain[k][1] == j) {
@@ -35,6 +40,7 @@ window.onload = function() {
                     }
                 }
 
+		// Update rain droplets and their trails
                 if (rainy) {
                     chars[i][j][0] = Math.round(Math.random() * 95 + 32);
                     chars[i][j][1] = 255;
@@ -46,11 +52,12 @@ window.onload = function() {
 
                 chars[i][j][1] = Math.max(chars[i][j][1] - 10, 0);
                 if (chars[i][j][1] < 20) {
-                    chars[i][j][0] = 127;
+                    chars[i][j][0] = 32;
                 }
             }
         }
 
+	// Move the rain down
         for (var i = 0; i < rain.length; i++) {
             rain[i][1] += 1;
             if (rain[i][1] > rows) {
@@ -61,6 +68,7 @@ window.onload = function() {
 
     setInterval(drawGrid, 100);
 
+    // Show status of database request
     if (window.location.hash) {
         console.log(window.location.hash);
         var hash = window.location.hash.substring(1);
@@ -70,4 +78,10 @@ window.onload = function() {
             document.getElementById('successIndicator').innerHTML = 'An error occurred; please try again later.';
         }
     }
+
+    // Translate moose
+    var text = document.getElementById('welcome-text');
+    var moose = document.getElementById('stan');
+    moose.style.mstransform = 'translate(-50%, ' + (-2 * text.offsetHeight).toString() + 'px)';
+    moose.style.transform = 'translate(-50%, ' + (-2 * text.offsetHeight).toString() + 'px)';
 }
