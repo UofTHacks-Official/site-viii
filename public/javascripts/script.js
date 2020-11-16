a6bb1d1b25da6090d3a35e3af4aee28bbc3da750
zmct <zmct99@gmail.com>
2020-11-16 18:12:19 -0500

    Made code rain reset on resize

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+function code_rain() {
     var canvas = document.getElementById('matrix');
     const ctx = canvas.getContext('2d');
     const w = screen.width;
@@ -18,7 +18,8 @@ window.onload = function() {
             chars[i][j] = [32, 255];
         }
     }
-    var rain = Array(10); // Array(Math.floor(cols / 10));
+
+    var rain = Array(5 * Math.floor(Math.log(cols)));
     for (var i = 0; i < rain.length; i++) {
         rain[i] = [Math.round(Math.random() * cols), Math.round(-1 * Math.random() * rows)];
     }
@@ -66,7 +67,15 @@ window.onload = function() {
         }
     }
 
-    setInterval(drawGrid, 100);
+    return setInterval(drawGrid, 100);
+}
+
+function setup() {
+    var interval = code_rain();
+    window.onresize = function() {
+        clearInterval(interval);
+        interval = code_rain();
+    }
 
     // Show status of database request
     if (window.location.hash) {
@@ -84,3 +93,5 @@ window.onload = function() {
     var moose = document.getElementById('stan');
     moose.style.bottom = text.offsetHeight.toString() + 'px';
 }
+
+window.onload = setup;
